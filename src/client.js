b1358bf343995967d12495de708ee8da15dcdf6d
erwanledoux <lawanledoux@gmail.com>
2017-08-06 12:04:04 +0200

    fixed window undefined

--- src/client.js
+++ src/client.js
@@ -2,7 +2,7 @@ import io from 'socket.io-client'
 
 export function getClient (config = {}) {
   const NAME_SPACE = config.NAME_SPACE ||
-  window && window.__CONFIG__ && window.__CONFIG__.NAME_SPACE ||
+  (typeof window !== 'undefined' && window.__CONFIG__ && window.__CONFIG__.NAME_SPACE) ||
   '/transactions'
   const url = config.url || ''
   const connectUrl = `${url}${NAME_SPACE}`
